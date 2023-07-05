eba39e62d73d9c51b802a723acffe6b29096938f
hiepvh8 <hiepvh8@gmail.com>
2023-07-05 21:58:34 +0700

    firstcommit by hiep initializr

---
+++ src/main/java/com/optimatech/digitmall/DigitmallApplication.java
@@ -0,0 +1,13 @@
+package com.optimatech.digitmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optimatech/digitmall/DigitmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimatech.digitmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
