7adedbc37613c28010a5b3f7bbfc4163461773a3
I'm_Cong <nguyenmanhcongptit@gmail.com>
2023-07-11 19:27:34 +0700

    customer feature P1

--- src/main/java/com/optimatech/digitmall/repository/CustomerRepository.java
+++ src/main/java/com/optimatech/digitmall/repository/CustomerRepository.java
@@ -1,27 +1,42 @@
 package com.optimatech.digitmall.repository;
 
 import com.optimatech.digitmall.model.entity.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.Optional;
 
+@Repository
+@Transactional // quản lí sự kiện tương tác với database
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
     //Dùng JPQL
-    @Query("UPDATE Customer SET  firstname = :firstname, lastname = :lastname, introduce = :introduce, birthday = : birthday ")
+    @Modifying // thông báo function có khả năng làm thay đổi dữ liệu
+    @Query("UPDATE Customer c SET  c.firstname = :firstname, c.lastname = :lastname," +
+            " c.introduce = :introduce, c.link =:link, c.birthday =:birthday WHERE c.id =:cusid")
     void updateInfor(@Param("firstname") String firstname,
                             @Param("lastname") String lastname,
                             @Param("introduce") String introduct,
-                            @Param("birthday")Date date);
+                            @Param("link") String link,
+                            @Param("birthday") Date date,
+                            @Param("cusid") Long id
+                            );
 
 
-    @Query("UPDATE Customer SET avatarImg  = :avturl")
-    void updateAvatar(@Param("avturl") String url);
+    @Modifying
+    @Query("UPDATE Customer c SET c.avatarImg  = :avturl WHERE c.id =:cusid")
+    void updateAvatar(@Param("avturl") String url,@Param("cusid") Long id);
 
-    @Query("UPDATE Customer SET backGroundImg = :url")
-    void updateBackGroundImg(@Param("url") String url);
+    @Modifying
+    @Query("UPDATE Customer c SET c.backGroundImg = :url WHERE c.id =:cusid")
+    void updateBackGroundImg(@Param("url") String url, @Param("cusid") Long id);
 
     @Query("SELECT c FROM Customer c WHERE c.id = :cusid")
     Optional<Customer> findById(@Param("cusid") Long id);
--- src/main/java/com/optimatech/digitmall/serviceimp/CustomerSeviveImp.java
+++ src/main/java/com/optimatech/digitmall/serviceimp/CustomerSeviveImp.java
@@ -1,31 +1,51 @@
 package com.optimatech.digitmall.serviceimp;
 
+import com.optimatech.digitmall.model.dto.AddressRequest;
 import com.optimatech.digitmall.model.dto.CustomerInforRequest;
 import com.optimatech.digitmall.model.dto.CustomerResponse;
+import com.optimatech.digitmall.model.entity.Account;
+import com.optimatech.digitmall.model.entity.Address;
+import com.optimatech.digitmall.model.entity.Cart;
 import com.optimatech.digitmall.model.entity.Customer;
+import com.optimatech.digitmall.repository.AddressRepository;
+import com.optimatech.digitmall.repository.CartRepository;
 import com.optimatech.digitmall.repository.CustomerRepository;
 import com.optimatech.digitmall.services.CustomerService;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CustomerSeviveImp implements CustomerService {
 
     private final CustomerRepository customerRepository;
     private final ImageServiceImp imageServiceImp;
+    private final CartRepository cartRepository;
+    private final AddressRepository addressRepository;
 
-    public CustomerSeviveImp(CustomerRepository customerRepository, ImageServiceImp imageServiceImp) {
+    public CustomerSeviveImp(CustomerRepository customerRepository,
+                             ImageServiceImp imageServiceImp,
+                             CartRepository cartRepository,
+                             AddressRepository addressRepository) {
         this.customerRepository = customerRepository;
         this.imageServiceImp = imageServiceImp;
+        this.cartRepository = cartRepository;
+        this.addressRepository = addressRepository;
     }
 
-    public boolean updateInfor(CustomerInforRequest customerInforRequest){
+    public boolean updateInfor(CustomerInforRequest customerInforRequest, Long cusid){
         try {
             customerRepository.updateInfor(customerInforRequest.getFirstname(),
                     customerInforRequest.getLastname(),
                     customerInforRequest.getIntroduce(),
-                    customerInforRequest.getBirthday());
+                    customerInforRequest.getLink(),
+                    customerInforRequest.getBirthday(),
+                    cusid
+            );
 
         } catch (Exception e){
             System.out.println(e.getMessage());
@@ -45,16 +65,59 @@ public class CustomerSeviveImp implements CustomerService {
 
     }
 
-    public boolean updateAvatar(@RequestParam("file") MultipartFile file){
+    public String xemTruocAvatar( MultipartFile file){
         String url = "";
         try {
             url = imageServiceImp.createPathUrlForImage(file);
         } catch (Exception e){
             System.out.println(e.getMessage());
-            return false;
+
         }
-        customerRepository.updateAvatar(url);
-        return true;
+        return url;
+    }
+
+    public void updateAvatar(Long cusid,String url){
+        customerRepository.updateAvatar(url,cusid);
+    }
+
+    public boolean addCart(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Account accountPrincipal = (Account) authentication.getPrincipal();
+        Long cusid = accountPrincipal.getCustomer().getId();
+        if(accountPrincipal.getCustomer().getCart().equals("")){
+            Cart cart = new Cart();
+            cartRepository.save(cart);
+            Optional<Customer> customer = customerRepository.findById(cusid);
+            if(customer.isPresent()){
+                customer.get().setCart(cart);
+                customerRepository.save(customer.get());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void createAddress(Long cusid, AddressRequest addressRequest){
+        Address address = new Address(addressRequest);
+        Optional<Customer> customer = customerRepository.findById(cusid);
+
+        if(customer.isPresent()){
+            if(address.getMainAddress()){ // kiểm tra nếu cust set địa chỉ làm địa chỉ chính
+                List<Address> list = addressRepository.findAllAddressByCustomer(customer.get());
+                for (Address a:list) {
+                    if(a.getMainAddress()){
+                        a.setMainAddress(false); // set lai địa chỉ cũ thành false để bắt đầu lưu thông
+                                                // tin địa chỉ mới
+                        addressRepository.save(a);
+                        break;
+                    }
+                }
+            }
+            address.setCustomer(customer.get());
+            addressRepository.save(address); // lưu địa chỉ mới
+        }
+
+
     }
 }
 
